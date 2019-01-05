ed28bec675a071d132216d11d5333616aababd63
Sophie Déziel <courrier@sophiedeziel.com>
2019-01-05 00:20:09 -0500

    Update Streamdeck.ino
    Utilisation de ma nouvelle matrice 3x5

--- Streamdeck.ino
+++ Streamdeck.ino
@@ -1,14 +1,15 @@
 #include <Keyboard.h>
 #include <Keypad.h>
 
-const byte ROWS = 2; //four rows
-const byte COLS = 2; //three columns
+const byte ROWS = 3; 
+const byte COLS = 5;
 char keys[ROWS][COLS] = {
-  {'1', '2'},
-  {'3', '4'}
+  {'1', '2', '3', '4', '5'},
+  {'6', '7', '8', '9', '0'},
+  {'a', 'b', 'c', 'd', 'e'},
 };
-byte rowPins[ROWS] = {3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {9,8,7}; 
+byte colPins[COLS] = {2,3,4,5,6}; 
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -25,6 +26,7 @@ void loop() {
 
 void keypadEvent(KeypadEvent key) {
   if (keypad.getState() == PRESSED) {
+    Serial.println(key);
     Keyboard.press(KEY_LEFT_CTRL);
     Keyboard.press(KEY_LEFT_SHIFT);
     switch (key) {
