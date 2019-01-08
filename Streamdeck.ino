c4d812fbb4e9168f9f374c22449c3854e6c515b5
Sophie Déziel <courrier@sophiedeziel.com>
2019-01-07 23:18:19 -0500

    Ajout des boutons pour partir le live et pour les raids
    Cependant, partir un raid est pas bien intégré. Il faut chercher comment on peut le faire via l'API de twitch ou de Streamlabs OBS.

--- Streamdeck.ino
+++ Streamdeck.ino
@@ -1,26 +1,50 @@
 #include <Keyboard.h>
 #include <Keypad.h>
 
-const byte ROWS = 3; 
+const byte ROWS = 3;
 const byte COLS = 5;
 char keys[ROWS][COLS] = {
   {'1', '2', '3', '4', '5'},
   {'6', '7', '8', '9', '0'},
   {'a', 'b', 'c', 'd', 'e'},
 };
-byte rowPins[ROWS] = {9,8,7}; 
-byte colPins[COLS] = {2,3,4,5,6}; 
+byte rowPins[ROWS] = {14, 15, 9};
+byte colPins[COLS] = {4, 5, 6, 10, 8};
+
+long last_raid;
+long last_live;
+bool live = false;
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup() {
   Serial.begin(115200);
   Keyboard.begin();
-  keypad.addEventListener(keypadEvent);  
+  keypad.addEventListener(keypadEvent);
+
+  pinMode(7, INPUT_PULLUP);
+  pinMode(A0, INPUT);
+  attachInterrupt(digitalPinToInterrupt(7), triggerRaid, RISING);
 }
 
 void loop() {
-  
+  if (millis() - last_live > 200) {
+    if (digitalRead(A0) == HIGH && live == false) {
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press(KEY_F19);
+
+      live = true;
+    } else if (digitalRead(A0) == LOW && live == true) {
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press(KEY_F20);
+      live = false;
+    }
+    delay(10);
+    Keyboard.releaseAll();
+    last_live = millis();
+  }
   keypad.getKey();
 }
 
@@ -44,6 +68,13 @@ void keypadEvent(KeypadEvent key) {
         break;
     }
   }
-  delay(100);
+  delay(10);
   Keyboard.releaseAll();
+}
+
+void triggerRaid() {
+  if (millis() - last_raid > 200) {
+    Serial.println("Le raid est partiiiiiiiiiii!");
+    last_raid = millis();
+  }
 }
