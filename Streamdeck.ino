6c5dacf7668b4db0feb45984480c8848bdabe190
Sophie Déziel <courrier@sophiedeziel.com>
2019-01-21 21:22:02 -0500

    Ajouter le module de temps

--- Streamdeck.ino
+++ Streamdeck.ino
@@ -2,11 +2,18 @@
 #include <Keypad.h>
 #include <TM1637Display.h>
 #include <TimeLib.h>
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
 
+#define CLOCK_CLK_PIN 8
+#define CLOCK_DIO_PIN 9
 #define UPTIME_CLK_PIN 2
 #define UPTIME_DIO_PIN 3
 #define RAID_SWITCH_PIN 7
 #define LIVE_SWITCH_PIN A0
+#define RTC_DIO_PIN A2
+#define RTC_CLK_PIN A3
+#define RTC_CE_PIN A1
 
 const byte ROWS = 3;
 const byte COLS = 5;
@@ -24,25 +31,56 @@ long last_live_toggle;
 bool live = false;
 time_t live_since = now();
 
-TM1637Display display(UPTIME_CLK_PIN, UPTIME_DIO_PIN);
+TM1637Display display_live(UPTIME_CLK_PIN, UPTIME_DIO_PIN);
+TM1637Display display_clock(CLOCK_CLK_PIN, CLOCK_DIO_PIN);
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+// Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+ThreeWire myWire(RTC_DIO_PIN, RTC_CLK_PIN, RTC_CE_PIN); 
+RtcDS1302<ThreeWire> Rtc(myWire);
 
 void setup() {
   Serial.begin(115200);
   Keyboard.begin();
-  keypad.addEventListener(keypadEvent);
+  Rtc.Begin();
+  // keypad.addEventListener(keypadEvent);
 
   pinMode(RAID_SWITCH_PIN, INPUT_PULLUP);
   pinMode(LIVE_SWITCH_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(RAID_SWITCH_PIN), triggerRaid, RISING);
-  display.setBrightness(0x33);
+  display_live.setBrightness(0x33);
+  display_clock.setBrightness(0x32);
+  
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
 
+  if (Rtc.GetIsWriteProtected())
+  {
+    Serial.println("RTC was write protected, enabling writing now");
+    Rtc.SetIsWriteProtected(false);
+  }
+
+  if (!Rtc.GetIsRunning())
+  {
+    Serial.println("RTC was not actively running, starting now");
+    Rtc.SetIsRunning(true);
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled)
+  {
+    Serial.println("RTC is older than compile time!  (Updating DateTime)");
+    Rtc.SetDateTime(compiled);
+  }
 
 }
 
 void loop() {
-  displayUptime();
+  display_liveUptime();
+  RtcDateTime now = Rtc.GetDateTime();
+
+  printDateTime(now);
 
   if (millis() - last_live_toggle > 200) {
     if (digitalRead(LIVE_SWITCH_PIN) == HIGH && live == false) {
@@ -53,36 +91,34 @@ void loop() {
 
     last_live_toggle = millis();
   }
+  delay(100);
 
-  keypad.getKey();
-
-
-
+  // keypad.getKey();
 }
 
-void keypadEvent(KeypadEvent key) {
-  if (keypad.getState() == PRESSED) {
-    Serial.println(key);
-    Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.press(KEY_LEFT_SHIFT);
-    switch (key) {
-      case '1':
-        Keyboard.press(KEY_F1);
-        break;
-      case '2':
-        Keyboard.press(KEY_F2);
-        break;
-      case '3':
-        Keyboard.press(KEY_F3);
-        break;
-      case '4':
-        Keyboard.press(KEY_F4);
-        break;
-    }
-  }
-  delay(10);
-  Keyboard.releaseAll();
-}
+// void keypadEvent(KeypadEvent key) {
+//  if (keypad.getState() == PRESSED) {
+// Serial.println(key);
+//    Keyboard.press(KEY_LEFT_CTRL);
+//    Keyboard.press(KEY_LEFT_SHIFT);
+//    switch (key) {
+//      case '1':
+//        Keyboard.press(KEY_F1);
+//        break;
+//      case '2':
+//        Keyboard.press(KEY_F2);
+//        break;
+//      case '3':
+//        Keyboard.press(KEY_F3);
+//        break;
+//      case '4':
+//        Keyboard.press(KEY_F4);
+//        break;
+//    }
+//  }
+ // delay(10);
+ // Keyboard.releaseAll();
+//} 
 
 void triggerRaid() {
   if (millis() - last_raid > 200) {
@@ -94,7 +130,7 @@ void triggerRaid() {
 void startLive() {
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press(KEY_LEFT_SHIFT);
-  keyboard.press(KEY_F19);
+  Keyboard.press(KEY_F19);
 
   live = true;
   live_since = now();
@@ -112,13 +148,13 @@ void endLive() {
   Keyboard.releaseAll();
 }
 
-void displayUptime() {
+void display_liveUptime() {
   uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
 
   if (live) {
     time_t t = now() - live_since;
 
-    display.showNumberDecEx(hour(t) * 100 + minute(t), 0x40, true);
+    display_live.showNumberDecEx(hour(t) * 100 + minute(t), 0x40, true);
   } else {
     uint8_t SEG_UNDEFINED[] = {
       SEG_G,
@@ -126,7 +162,11 @@ void displayUptime() {
       SEG_G,
       SEG_G,
     };
-    display.setSegments(SEG_UNDEFINED);
+    display_live.setSegments(SEG_UNDEFINED);
   }
+}
 
+void printDateTime(const RtcDateTime& dt)
+{
+  display_clock.showNumberDecEx(dt.Hour() * 100 + dt.Minute(), 0x40, true);
 }
